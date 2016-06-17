df3f7ed1144ba6a52c3ebcfbf018f79702b2b092
Wei Wang <wang0109@gmail.com>
2016-06-16 21:43:32 -0400

    try chris 2T offset

--- lib/asan/asan_mapping.h
+++ lib/asan/asan_mapping.h
@@ -127,7 +127,7 @@
 static const u64 kDefaultShadowScale = 3;
 static const u64 kDefaultShadowOffset32 = 1ULL << 29;  // 0x20000000
 static const u64 kDefaultShadowOffset64 = 1ULL << 44;
-static const u64 kDefaultShort64bitShadowOffset = 0x200000000000;  // 32T
+static const u64 kDefaultShort64bitShadowOffset = 0x20000000000;  // Not 32T, 2T from Chirs..
 static const u64 kIosShadowOffset32 = 1ULL << 30;  // 0x40000000
 static const u64 kIosShadowOffset64 = 0x120200000;
 static const u64 kIosSimShadowOffset32 = 1ULL << 30;
