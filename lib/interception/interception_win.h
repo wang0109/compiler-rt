9a94bfec8298fc06e03454a2723788266a633aa5
Wei Wang <wang0109@gmail.com>
2016-06-19 19:49:44 -0400

    debug: fix namespace

--- lib/interception/interception_win.h
+++ lib/interception/interception_win.h
@@ -65,7 +65,7 @@ void TestOnlyReleaseTrampolineRegions();
 #if defined(INTERCEPTION_DYNAMIC_CRT)
 #define INTERCEPT_FUNCTION_WIN(func)                                          \
   do {                                                                        \
-    ::__asan::Report("intercepting(win, dyn crt):" #func "\n");                         \
+    ::__sanitizer::Report("intercepting(win, dyn crt):" #func "\n");                         \
     ::__interception::OverrideFunction(#func,                                 \
                                        (::__interception::uptr)WRAP(func),    \
                                        (::__interception::uptr *)&REAL(func)) \
@@ -73,7 +73,7 @@ void TestOnlyReleaseTrampolineRegions();
 #else
 #define INTERCEPT_FUNCTION_WIN(func)                                          \
   do {                                                                        \
-    ::__asan::Report("intercepting(win, non dyn crt):" #func "\n");                     \
+    ::__sanitizer::Report("intercepting(win, non dyn crt):" #func "\n");                     \
     ::__interception::OverrideFunction((::__interception::uptr)func,          \
                                        (::__interception::uptr)WRAP(func),    \
                                        (::__interception::uptr *)&REAL(func)) \
