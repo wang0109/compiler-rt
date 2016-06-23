6c4439466e5e37f2c4b44c3935bc6846d62f513f
Wei Wang <wang0109@gmail.com>
2016-06-23 16:38:05 -0400

    larger buffer

--- lib/asan/asan_allocator.cc
+++ lib/asan/asan_allocator.cc
@@ -766,7 +766,7 @@ void InitializeAllocator(const AllocatorOptions &options) {
   volatile char* y = 0;
 
   if (x != 0) {
-#define ZLEN 20
+#define ZLEN 40
     char z [ZLEN] = {};
     int i;
     for (i = 0; i < ZLEN; ++i) {
