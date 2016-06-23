21896b8541d8b639061df5dd33fffbae3a109097
Wei Wang <wang0109@gmail.com>
2016-06-23 15:44:25 -0400

    try use constexpr to get type

--- lib/asan/asan_allocator.cc
+++ lib/asan/asan_allocator.cc
@@ -723,6 +723,37 @@ StackTrace AsanChunkView::GetAllocStack() {
 StackTrace AsanChunkView::GetFreeStack() {
   return GetStackTraceFromId(GetFreeStackId());
 }
+// Compile time type checking
+//http://stackoverflow.com/questions/35941045/can-i-obtain-c-type-names-in-a-constexpr-way/35943472#35943472
+struct string_view
+{
+    char const* data;
+    std::size_t size;
+};
+
+template<class T>
+constexpr string_view get_name()
+{
+    char const* p = __PRETTY_FUNCTION__;
+    while (*p++ != '=');
+    for (; *p == ' '; ++p);
+    char const* p2 = p;
+    int count = 1;
+    for (;;++p2)
+    {
+        switch (*p2)
+        {
+        case '[':
+            ++count;
+            break;
+        case ']':
+            --count;
+            if (!count)
+                return {p, std::size_t(p2 - p)};
+        }
+    }
+    return {};
+}
 
 void InitializeAllocator(const AllocatorOptions &options) {
   // FIXME(wwchrome): Debug only.
@@ -732,6 +763,8 @@ void InitializeAllocator(const AllocatorOptions &options) {
   // only include typeinfo will trigger Asan init calls itself?
   /* volatile char* tn = (char*)typeid(instance).name(); */
   // FIXME(wwchrome): Debug only.
+  constexpr auto n = get_name<instance>();
+  volatile char* x = n;
   __debugbreak();
   instance.Initialize(options);
 }
