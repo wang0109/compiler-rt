7192f2e6d60ed33767b59a6ea02b848436a20163
Wei Wang <wang0109@gmail.com>
2016-06-23 16:47:55 -0400

    try some random

--- lib/asan/asan_allocator.cc
+++ lib/asan/asan_allocator.cc
@@ -770,7 +770,7 @@ void InitializeAllocator(const AllocatorOptions &options) {
 /* #define ZLEN 80 */
     char z [200] = {};
     int i;
-    for (i = 0; i < ZLEN; ++i) {
+    for (i = 0; i < ZLEN-5; ++i) {
       z[i] = x[i];
     }
     if (z[0] > '0') {
